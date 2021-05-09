626d8fb1045dc401915b1c9a65e4f0b0820b7c6a
Samuel Polakovič <samuel.polakovic@studentstc.sk>
2021-05-09 13:46:49 +0200

    end of 4-attribute-binding

--- web/index.js
+++ web/index.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
 		return {
 			productName: 'Book a Cruise to the Moon',
 			productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+			imageSource: './media/sample.jpg',
 			// additional properties later
+
+			styleObject: {
+				'height': '200px'
+			}
 		}
 	},
 });
